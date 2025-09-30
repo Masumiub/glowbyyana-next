2d49e88b7fc1d8ab888ea7238246d85dc39e8869
Md. Masum Musfique <musfiquemasum8@gmail.com>
2025-09-30 14:00:07 +0600

    loading added

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageLoading from './components/ui/PageLoading'
+
+export default function loading() {
+  return (
+    <div>
+      <PageLoading></PageLoading>
+    </div>
+  )
+}
